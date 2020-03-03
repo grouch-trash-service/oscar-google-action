2597b447b552597a146253a57d0b373bc7d4ad03
Matt Porter <matthew.js.porter@gmail.com>
2020-03-02 22:35:11 -0500

    Moving intent operations to firebase function

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {dialogflow} = require('actions-on-google');
+const functions = require('firebase-functions');
+
+const app = dialogflow({debug: true});
+
+app.intent('Default Welcome Intent', (conv) => {
+    conv.close('Hello. Trash pickup will be on Tuesday... Now SCRAM!');
+ });
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
