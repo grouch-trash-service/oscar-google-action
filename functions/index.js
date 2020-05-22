8ac262703913c31e1409fb831702780aefc198a0
Matt Porter <matthew.js.porter@gmail.com>
2020-05-22 14:47:50 -0400

    reverting accidental change to index.js

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,13 @@
 'use strict';
 
-const {Permission, dialogflow} = require('actions-on-google');
+const {dialogflow} = require('actions-on-google');
 const functions = require('firebase-functions');
 
 const app = dialogflow({debug: true});
 
 app.intent('Default Welcome Intent', (conv) => {
-    conv.ask(new Permission({
-        context: 'Hi there, to get to know you better',
-        permissions: 'NAME',
-    }));
     conv.close('Hello. Trash pickup will be on Tuesday... Now SCRAM!');
- });
+});
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
 exports.app = app;
