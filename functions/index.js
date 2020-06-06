83a1bc6c0e180813815684cf52ac1eebd99356cb
Matt Porter <matthew.js.porter@gmail.com>
2020-06-06 01:39:08 -0400

    Updating to use grouch-message-service to retrieve the message.

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,18 @@ const {dialogflow} = require('actions-on-google');
 const functions = require('firebase-functions');
 
 const app = dialogflow({debug: true});
+const config = require('config');
+const factory = require('./grouch/api-client-factory');
+const {GrouchMessageService} = require('./grouch/grouch-message-service');
+
+const apiClient = factory
+    .build(config.get('grouch-message-service.url'),
+        config.get('grouch-message-service.token'));
+const grouchMessageService = new GrouchMessageService(apiClient);
 
 app.intent('Default Welcome Intent', async (conv) => {
-    conv.close('Hello. Trash pickup will be on Tuesday... Now SCRAM!');
+    const message = await grouchMessageService.getMessage();
+    conv.close(message.text);
 });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
--- functions/index.test.js
+++ functions/index.test.js
@@ -1,16 +1,23 @@
 const app = require('./index').app;
 const sinon = require('sinon');
+const {GrouchMessageService} = require('./grouch/grouch-message-service');
 
 describe('Describe Google Action', ()=>{
    describe('I ask when trash pickup is', () => {
-       it('should return trash pickup information', () => {
+       it('should return trash pickup information', async () => {
+           const expectedMessage = 'Hello. Trash pickup' +
+               ' will be on Tuesday... Now SCRAM!';
+           sinon.stub(GrouchMessageService.prototype,
+               'getMessage').resolves({text: expectedMessage});
            const conv = {
                close: sinon.stub(),
            };
-           app._handlers.intents['Default Welcome Intent'].call('', conv);
+
+           await app._handlers.intents['Default Welcome Intent'].call('', conv);
+
            sinon.assert.calledOnce(conv.close);
            sinon.assert.calledWith(conv.close,
-               'Hello. Trash pickup will be on Tuesday... Now SCRAM!');
+               expectedMessage);
        });
    });
 });
