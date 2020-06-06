610a9a649583136beb16cc05d4106be126f4cf88
Matt Porter <matthew.js.porter@gmail.com>
2020-06-06 08:21:40 -0400

    Adding CircuitBreaker options

--- functions/grouch/grouch-message-service.js
+++ functions/grouch/grouch-message-service.js
@@ -10,16 +10,27 @@ class GrouchMessageService {
      */
     constructor(messageApiClient) {
         this.messageApiClient = messageApiClient;
+        this.options = {
+            timeout: 5000,
+            errorThresholdPercentage: 80,
+            resetTimeout: 3000,
+        };
     }
-
     /**
      * Returns a message from the service
      * @return {Promise<Message>}
      */
     async getMessage() {
-        const breaker = new CircuitBreaker(this._fetchMessage, {});
+        const breaker = new CircuitBreaker(this._fetchMessage, this.options);
         return await breaker.fire(this.messageApiClient);
     }
+
+    /**
+     *  Fetches message from the service
+     * @param {MessageApi} messageApiClient
+     * @return {Promise<Message>}
+     * @private
+     */
     async _fetchMessage(messageApiClient) {
         let message = await messageApiClient.getMessage();
         return message.body;
